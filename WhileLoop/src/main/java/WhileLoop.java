d6f42589425dca390e02e2ef8f09ca6bcae327dd
Alan Leonel Perez <004862867@coyote.csusb.edu>
2022-12-22 22:13:45 +0000

    Saved progress on WhileLoop 2022-12-22 22:13:45 +0000

--- WhileLoop/src/main/java/WhileLoop.java
+++ WhileLoop/src/main/java/WhileLoop.java
@@ -35,9 +35,11 @@ public class WhileLoop {
     public int whileExample(int end){
         int sum = 0;
         int i = 0;
-
-//        code here
-
+        while(i < end)
+        {
+            sum = sum + i;
+            i = i + 1;
+        }
         return sum;
     }
 }
