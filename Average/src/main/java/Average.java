03dc7a8f3c03c9137fc08e5dd992c128c6d83444
Alan Leonel Perez <004862867@coyote.csusb.edu>
2023-01-06 10:50:58 +0000

    Saved progress on Average 2023-01-06 10:50:58 +0000

--- Average/src/main/java/Average.java
+++ Average/src/main/java/Average.java
@@ -18,6 +18,14 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+        }
+        double doubleSum = (double)sum;
+        double numsLength = (double) nums.length;
+        double average = (doubleSum / numsLength) ;
+        
+        return average;
     }
 }
