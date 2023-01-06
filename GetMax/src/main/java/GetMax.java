e969ca04a014ecd67fb0176221d38a14c50cf79b
Alan Leonel Perez <004862867@coyote.csusb.edu>
2023-01-06 10:58:27 +0000

    Saved progress on GetMax 2023-01-06 10:58:27 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int largestInt = arr[0];
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] > largestInt){
+                largestInt = arr[i];
+            }
+        }
+        return largestInt;
     }
 }
