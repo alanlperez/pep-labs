a85074013d24f638231283b82cb1b6c4faf4b9cd
Alan Leonel Perez <004862867@coyote.csusb.edu>
2023-01-20 03:50:28 +0000

    Saved progress on CreatingEndpoints 2023-01-20 03:50:28 +0000

--- CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
+++ CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
@@ -12,9 +12,20 @@ public class JavalinSingleton {
      * Note: Please refer to the "CreatingEndpoints.MD" file for more assistance if needed.
      */
     public static Javalin getInstance(){
-        Javalin app = Javalin.create();
+        Javalin app = Javalin.create().start(9000);
         
-        //write endpoint here
+            // HTTP GET REQUEST to http://localhost:9000/get-hello
+            app.get("/get-hello", ctx -> {
+            //logic to be executed when this endpoint is hit
+            ctx.result("Hello World");
+            });
+
+
+            // HTTP POST REQUEST to http://localhost:9000/post-hello
+            app.post("/post-hello", ctx -> {
+            //logic to be executed when this endpoint is hit
+            ctx.result("Hello World");
+    });
 
         return app;
     }
