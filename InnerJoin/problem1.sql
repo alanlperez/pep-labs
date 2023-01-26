fdf723fbf40c45c90e02dfd7d8ed34d22ab42485
Alan Leonel Perez <004862867@coyote.csusb.edu>
2023-01-26 09:35:16 +0000

    Saved progress on InnerJoin 2023-01-26 09:35:16 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,1 +1,1 @@
-SELECT id, student_name FROM class INNER JOIN student ON class.class_title = student.class_title WHERE class.teacher_name = "Ms. Lovelace";
+SELECT student.id, student_name FROM class INNER JOIN student ON class.class_title = student.class_title WHERE class.teacher_name = 'Ms. Lovelace';
